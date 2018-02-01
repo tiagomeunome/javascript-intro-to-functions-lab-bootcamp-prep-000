13180e015601844e730053d68713a68e4db3fa85
tiago m <t.iagomeunome@gmail.com>
2018-02-01 18:50:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
     }
   else if (string === string.toUpperCase()){ return "YES INDEED!";}
-  else if (string === "I love you, Grandma") {return "I love you, too.";}
+  else {return "I love you, too.";}
 }
